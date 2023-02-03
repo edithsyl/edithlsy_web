d0c917f08cb50525567a5fef53927156a8cbaf3d
syleung <syleung1220@gmail.com>
2023-02-03 09:47:14 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,56 @@
 // https://www.youtube.com/watch?v=ubHwScDfRQA
+// list dependences
 const { src, dest, watch, series } = require("gulp");
-const sass = require("gulp-sass");
+const sass = require("gulp-sass")(require("sass"));
 const prefix = require("gulp-autoprefixer");
 const minify = require("gulp-clean-css");
 const terser = require("gulp-terser");
 const imagemin = require("gulp-imagemin");
 const imagewebp = require("gulp-webp");
 
+// create functions
+// scss
 function compilescss() {
-  return;
+  return src("scss/*.scss")
+    .pipe(sass())
+    .pipe(prefix("last 2 versions"))
+    .pipe(minify())
+    .pipe(dest("/dist"));
 }
 
-function defaultTask(cb) {
-  cb();
+// javascript
+function jsmin() {
+  return src("js/*.js").pipe(dest("/dist/js"));
 }
 
-exports.default = defaultTask;
+// images
+function optimizeimg() {
+  return src("app/assets/images/*{jpg,png}")
+    .pipe(
+      imagemin([
+        imagemin.mozjpeg({ quality: 80, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 2 }),
+      ])
+    )
+    .pipe(dest("dist/images"));
+}
+// webp images
+function webpImage() {
+  return src("app/assets/images/*{jpg,png}")
+    .pipe(imagewebp())
+    .pipe(dest("dist/images"));
+}
+
+// create watchtask
+function watchtask() {
+  watch("app/assets/scss/*.scss", compilescss);
+  watch("app/assets/js/*.js", jsmin);
+  watch("app/assets/images/*{jpg,png}", optimizeimg);
+  watch("app/assets/images/*{jpg,png}", webpImage);
+}
 
 //Default gulp task
-exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+exports.default = series(compilescss, jsmin, optimizeimg, webpImage, watchtask);
 
 //Build gulp task
-exports.build = series(scssTask, jsTask);
+exports.build = series(compilescss, jsmin);
