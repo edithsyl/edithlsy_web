598937484825927eb51feea531fe837f918ddb6d
syleung <syleung1220@gmail.com>
2023-02-04 13:52:45 +0800

    project section updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,66 @@
 // https://www.youtube.com/watch?v=ubHwScDfRQA
 // list dependences
+import gulp from "gulp";
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const prefix = require("gulp-autoprefixer");
 const minify = require("gulp-clean-css");
-const terser = require("gulp-terser");
+// const terser = require("gulp-terser");
 const imagemin = require("gulp-imagemin");
 const imagewebp = require("gulp-webp");
 
 // create functions
 // scss
 function compilescss() {
-  return src("scss/*.scss")
+  return gulp
+    .src("scss/*.scss")
     .pipe(sass())
     .pipe(prefix("last 2 versions"))
     .pipe(minify())
-    .pipe(dest("/dist"));
+    .pipe(gulp.dest("/dist"));
 }
 
 // javascript
 function jsmin() {
-  return src("js/*.js").pipe(dest("/dist/js"));
+  return gulp.src("js/*.js").pipe(gulp.dest("/dist/js"));
 }
 
 // images
 function optimizeimg() {
-  return src("app/assets/images/*{jpg,png}")
+  return gulp
+    .src("app/assets/images/*{jpg,png}")
     .pipe(
       imagemin([
         imagemin.mozjpeg({ quality: 80, progressive: true }),
         imagemin.optipng({ optimizationLevel: 2 }),
       ])
     )
-    .pipe(dest("dist/images"));
+    .pipe(gulp.dest("dist/images"));
 }
 // webp images
 function webpImage() {
-  return src("app/assets/images/*{jpg,png}")
+  return gulp
+    .src("app/assets/images/*{jpg,png}")
     .pipe(imagewebp())
-    .pipe(dest("dist/images"));
+    .pipe(gulp.dest("dist/images"));
 }
 
 // create watchtask
 function watchtask() {
-  watch("app/assets/scss/*.scss", compilescss);
-  watch("app/assets/js/*.js", jsmin);
-  watch("app/assets/images/*{jpg,png}", optimizeimg);
-  watch("app/assets/images/*{jpg,png}", webpImage);
+  gulp.watch("app/assets/scss/*.scss", compilescss);
+  gulp.watch("app/assets/js/*.js", jsmin);
+  gulp.watch("app/assets/images/*{jpg,png}", optimizeimg);
+  gulp.watch("app/assets/images/*{jpg,png}", webpImage);
 }
 
 //Default gulp task
-exports.default = series(compilescss, jsmin, optimizeimg, webpImage, watchtask);
+exports.default = gulp.series(
+  compilescss,
+  jsmin,
+  optimizeimg,
+  webpImage,
+  watchtask
+);
 
 //Build gulp task
-exports.build = series(compilescss, jsmin);
+exports.build = gulp.series(compilescss, gulp.parallel(jsmin));
